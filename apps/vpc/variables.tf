861f477fff6b28554e95d2cc76f8614c038a5f1e
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-22 14:19:43 +0000

    vpc

--- apps/vpc/variables.tf
+++ apps/vpc/variables.tf
@@ -15,3 +15,7 @@ variable "private_subnet_cidr_blocks" {
     type = list(string)
     default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
+
+variable "aws_region" {}
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
