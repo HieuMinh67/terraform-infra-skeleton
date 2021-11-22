992f08ca8d12b33eec72ed72cece3e21aa24297f
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-22 13:25:40 +0000

    vpc

--- apps/vpc/variables.tf
+++ apps/vpc/variables.tf
@@ -1,6 +1,17 @@
 variable "app_name" {}
-variable "cidr_block" {}
 variable "bounded_context" {}
 variable "enable_nat_gateway" {
   default = false
+}
+
+variable "cidr_block" {
+    default = "10.0.0.0/16"
+}
+variable "public_subnet_cidr_blocks" {
+  type    = list(string)
+  default = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+variable "private_subnet_cidr_blocks" {
+    type = list(string)
+    default = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
 }
--- apps/vpc/vpc.tf
+++ apps/vpc/vpc.tf
@@ -5,8 +5,8 @@ module "vpc" {
   name                 = var.app_name
   cidr                 = var.cidr_block
   azs                  = data.aws_availability_zones.available.names
-  private_subnets      = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+  private_subnets      = var.private_subnet_cidr_blocks
+  public_subnets       = var.public_subnet_cidr_blocks
   enable_nat_gateway   = var.enable_nat_gateway
   single_nat_gateway   = true
   enable_dns_hostnames = true
@@ -15,38 +15,39 @@ module "vpc" {
     "Name" = "${var.app_name}-default-sg"
   }
 
-  public_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/elb"                      = "1"
-    "type"                                        = "public"
-  }
+  # let Kops decide what to tag
+  # public_subnet_tags = {
+  #   "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  #   "kubernetes.io/role/elb"                      = "1"
+  #   "type"                                        = "public"
+  # }
 
-  private_subnet_tags = {
-    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
-    "kubernetes.io/role/internal-elb"             = "1"
-    "type"                                        = "private"
-  }
-}
-
-resource "aws_ram_resource_share" "this" {
-  name                      = var.app_name
-  allow_external_principals = false
+  # private_subnet_tags = {
+  #   "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+  #   "kubernetes.io/role/internal-elb"             = "1"
+  #   "type"                                        = "private"
+  # }
 }
 
-resource "aws_ram_resource_association" "this" {
-  for_each           = toset(concat(module.vpc.private_subnet_arns, module.vpc.public_subnet_arns))
-  resource_arn       = each.value
-  resource_share_arn = aws_ram_resource_share.this.arn
-}
-resource "aws_ram_principal_association" "sinh_vien_lam_web" {
-  principal          = "484883180270"
-  resource_share_arn = aws_ram_resource_share.this.arn
-}
+# resource "aws_ram_resource_share" "this" {
+#   name                      = var.app_name
+#   allow_external_principals = false
+# }
+
+# resource "aws_ram_resource_association" "this" {
+#   for_each           = toset(concat(module.vpc.private_subnet_arns, module.vpc.public_subnet_arns))
+#   resource_arn       = each.value
+#   resource_share_arn = aws_ram_resource_share.this.arn
+# }
+# resource "aws_ram_principal_association" "sinh_vien_lam_web" {
+#   principal          = "484883180270"
+#   resource_share_arn = aws_ram_resource_share.this.arn
+# }
 
 
 data "aws_availability_zones" "available" {
 }
 
-locals {
-  cluster_name = "test_cluster"
-}
+# locals {
+#   cluster_name = var.app_category
+# }
