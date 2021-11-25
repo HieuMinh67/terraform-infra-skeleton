8b966cc3c77f3ff59af0de243ecbbf7d8eceed47
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-25 08:18:16 +0000

    nat

--- apps/vpc/nat.tf
+++ apps/vpc/nat.tf
@@ -1,6 +1,6 @@
 
 module "nat" {
-  source = "nat_instance"
+  source = "./nat_instance"
   private_ips       = ["10.0.4.10"]
   security_group_id = var.nat_instance_sg_id
   aws_region        = var.aws_region
