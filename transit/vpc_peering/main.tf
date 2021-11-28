3e363a6d539a814d94b577486a3497bb66da678d
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-28 10:05:30 +0000

    Transit

--- transit/vpc_peering/main.tf
+++ transit/vpc_peering/main.tf
@@ -4,13 +4,13 @@ resource "aws_vpc_peering_connection" "requestor" {
   peer_vpc_id   = var.peer_vpc_id
   vpc_id        = var.vpc_id
 
-  accepter {
-    allow_remote_vpc_dns_resolution = true
-  }
+  # accepter {
+  #   allow_remote_vpc_dns_resolution = true
+  # }
 
-  requester {
-    allow_remote_vpc_dns_resolution = true
-  }
+  # requester {
+  #   allow_remote_vpc_dns_resolution = true
+  # }
 
   tags = {
     Name = "VPC Peering from ${var.vpc_id} to ${var.peer_vpc_id}"
