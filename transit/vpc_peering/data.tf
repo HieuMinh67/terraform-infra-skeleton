e7372b883d7b0311ee355b2d8e7e793305a46862
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-11-29 11:29:37 +0000

    Transit

--- transit/vpc_peering/data.tf
+++ transit/vpc_peering/data.tf
@@ -2,12 +2,23 @@ data "aws_route_table" "private" {
   subnet_id = tolist(data.aws_subnet_ids.private.ids)[0]
 }
 
-
 data "aws_subnet_ids" "private" {
   vpc_id = var.vpc_id
 
   tags = {
     Tier = "Private"
+  }
+}
+
+data "aws_route_table" "public" {
+  subnet_id = tolist(data.aws_subnet_ids.public.ids)[0]
+}
+
+data "aws_subnet_ids" "public" {
+  vpc_id = var.vpc_id
+
+  tags = {
+    Tier = "Public"
   }
 }
 
@@ -32,5 +43,18 @@ data "aws_subnet_ids" "peer_private" {
 
   tags = {
     Tier = "Private"
+  }
+}
+
+
+data "aws_route_table" "peer_public" {
+  subnet_id = tolist(data.aws_subnet_ids.peer_public.ids)[0]
+}
+
+data "aws_subnet_ids" "peer_public" {
+  vpc_id = var.peer_vpc_id
+
+  tags = {
+    Tier = "Public"
   }
 }
