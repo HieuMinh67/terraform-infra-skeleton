10928fab006d5ec63c3af07894f5f469131aa2b6
EC2 Default User <ec2-user@ip-172-31-35-75.ap-southeast-1.compute.internal>
2021-12-10 14:21:37 +0000

    Github Actions

---
+++ github/runner/runner.tf
@@ -0,0 +1,40 @@
+module "github-runner" {
+  source  = "philips-labs/github-runner/aws"
+  version = var.version
+
+  aws_region = var.aws_region
+  vpc_id     = var.vpc_id
+  subnet_ids = var.subnet_ids
+
+  environment = var.environment
+
+  github_app = {
+    key_base64     = var.github_app_key_base64
+    id             = var.github_app_id
+    webhook_secret = var.github_app_webhook_secret
+  }
+
+  runner_extra_labels         = var.runner_extra_labels
+  input_lambda_s3_bucket      = var.input_lambda_s3_bucket
+  webhook_lambda_s3_key       = var.webhook_lambda_s3_key
+  syncer_lambda_s3_key        = var.syncer_lambda_s3_key
+  runners_lambda_s3_key       = var.runners_lambda_s3_key
+  enable_organization_runners = true
+  
+  # configure your pre-built AMI
+  enabled_userdata = var.enabled_userdata
+  ami_filter       = { name = [var.ami_filter_by_name] }
+  ami_owners       = [var.ami_owner_id]
+
+  # enable access to the runners via SSM
+  enable_ssm_on_runners = var.enable_ssm_on_runners
+
+  # override delay of events in seconds
+  delay_webhook_event = var.delay_webhook_event
+
+  # override scaling down
+  scale_down_schedule_expression = "cron(* * * * ? *)"
+
+  create_service_linked_role_spot = var.create_service_linked_role_spot
+  
+}
---
+++ github/runner/variables.tf
@@ -0,0 +1,35 @@
+variable "github_app_key_base64" {}
+variable "github_app_id" {}
+variable "input_lambda_s3_bucket" {}
+variable "webhook_lambda_s3_key" {}
+variable "syncer_lambda_s3_key" {}
+variable "runners_lambda_s3_key" {}
+
+variable "github_app_key_base64" {}
+variable "github_app_id" {}
+variable "github_app_webhook_secret" {}
+
+variable "version" {}
+variable "aws_region" {}
+variable "vpc_id" {}
+variable "subnet_ids" {}
+variable "environment" {}
+
+variable "ami_filter_by_name" {}
+variable "ami_owner_id" {}
+
+variable "create_service_linked_role_spot" {
+    default = true
+}
+
+variable "enable_ssm_on_runners" {
+    default = true
+}
+
+variable "delay_webhook_event" {
+    default = 5
+}
+
+variable "enabled_userdata" {
+    default = false
+}
