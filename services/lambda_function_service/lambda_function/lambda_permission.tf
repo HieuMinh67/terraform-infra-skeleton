8840d7398a95106374f1dc8d72966a3d640ed811
hieuminh <minhhieu060799@gmail.com>
2023-04-12 11:07:45 +0700

    update json invoke to lambda

--- services/lambda_function_service/lambda_function/lambda_permission.tf
+++ services/lambda_function_service/lambda_function/lambda_permission.tf
@@ -26,7 +26,7 @@ resource "aws_cloudwatch_event_rule" "cron_job" {
 resource "aws_cloudwatch_event_target" "trigger_lambda_on_schedule" {
   arn   = aws_lambda_function.this.arn
   rule  = aws_cloudwatch_event_rule.cron_job.name
-  input = "{\"account_id\": \"${var.target_account["account_id"]}\", \"access_key\": \"${var.target_account["access_key"]}\", \"secret_key\": \"${var.target_account["secret_key"]}\"}"
+  input = "{\"account_id\": \"${var.target_account["account_id"]}\", \"access_key\": \"${var.target_account["access_key"]}\", \"secret_key\": \"${var.target_account["secret_key"]}\", \"iam_username\": \"${var.target_account["iam_username"]}\"}"
 }
 
 resource "aws_lambda_permission" "allow-cloudwatch-to-call-split-lambda" {
