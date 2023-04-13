8a48a01e6777907a051dfa696ff7a50245a8bef3
Minh-Hieu Pham <43398131+HieuMinh67@users.noreply.github.com>
2023-04-13 17:57:47 +0700

    downgrade s3 bucket versioning (#5)

--- s3/bucket/main.tf
+++ s3/bucket/main.tf
@@ -1,10 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket = "${var.aws_account_id}-${var.aws_region}-${var.suffix_name}"
-}
-
-resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.this.id
-  versioning_configuration {
-    status = var.enable_versioning ? "Enabled" : "Disabled"
+   versioning {
+    enabled = var.enable_versioning
   }
 }
