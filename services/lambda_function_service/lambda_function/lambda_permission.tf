08bb6013c49535f63727dac9e686a8a23a404041
Minh-Hieu Pham <43398131+HieuMinh67@users.noreply.github.com>
2023-04-18 14:38:05 +0700

    fix aws_cloudwatch_event_target, aws_lambda_permission to get latest aws_cloudwatch_event_rule (#7)

--- services/lambda_function_service/lambda_function/lambda_permission.tf
+++ services/lambda_function_service/lambda_function/lambda_permission.tf
@@ -25,7 +25,7 @@ resource "aws_cloudwatch_event_rule" "cron_job" {
 
 resource "aws_cloudwatch_event_target" "trigger_lambda_on_schedule" {
   count = var.lambda_schedule_expression != "" ? 1 : 0
-  arn   = aws_lambda_function.this.arn
+  arn   = aws_lambda_function.this[0].arn
   rule  = aws_cloudwatch_event_rule.cron_job.name
   input = "{\"account_id\": \"${var.target_account["account_id"]}\", \"access_key\": \"${var.target_account["access_key"]}\", \"secret_key\": \"${var.target_account["secret_key"]}\", \"iam_username\": \"${var.target_account["iam_username"]}\"}"
 }
@@ -34,7 +34,7 @@ resource "aws_lambda_permission" "allow-cloudwatch-to-call-split-lambda" {
   count = var.lambda_schedule_expression != "" ? 1 : 0
   statement_id  = "AllowExecutionFromCloudWatch"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.this.function_name
+  function_name = aws_lambda_function.this[0].function_name
   principal     = "events.amazonaws.com"
   source_arn    = aws_cloudwatch_event_rule.cron_job.arn
 }
